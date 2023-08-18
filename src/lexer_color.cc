43ca6b10d00c5f584ca5f4d098986c37277d293d
oldlonecoder <serge.lussier@oldlonecoder.club>
2023-08-18 17:24:48 -0400

    lexer_color : Reduced redundant code.

--- src/lexer_color.cc
+++ src/lexer_color.cc
@@ -183,48 +183,13 @@ rem::code lexer_color::operator<<(const std::string& aSource)
     lex.config() = { aSource.c_str(),  & tokens}; // oop!!!
     rem::code E = lex();
     if (E != rem::accepted) return E;
+    return process(aSource, tokens);
 
-
-    //...
-    for (auto const& Token : tokens)
-    {
-        _color.clear();
-        _color = Token.c == xio::mnemonic::Noop ? attr<chattr::format::ansi256>::fg(lexer_color::Types[Token.t]) :
-            _color = attr<chattr::format::ansi256>::fg(lexer_color::affine_db[Token.c]);
-
-        Spacing = _color.length();
-        if (!_color.empty())
-        {
-            _product_data.insert(Token.mLoc.offset + Offset, _color);
-            Offset += Spacing;
-        }
-    }
-    return rem::ok;
 }
 
 rem::code lexer_color::operator<<(const lexer::config_data &cfg)
 {
-    std::string _color;
-    _product_data = cfg.Source;
-
-    size_t Spacing = 0, // Current color String::Length();
-           Offset = 0; // Cummulative ( Offset += Spacing )
-
-    for (auto const& Token : *cfg.Tokens)
-    {
-        _color.clear();
-        _color = Token.c == xio::mnemonic::Noop ? attr<chattr::format::ansi256>::fg(lexer_color::Types[Token.t]) :
-            _color = attr<chattr::format::ansi256>::fg(lexer_color::affine_db[Token.c]);
-
-        Spacing = _color.length();
-        if (!_color.empty())
-        {
-            _product_data.insert(Token.mLoc.offset + Offset, _color);
-            Offset += Spacing;
-        }
-    }
-    return rem::ok;
-    return rem::accepted;
+    return process(cfg.Source, *cfg.Tokens);
 }
 
 
