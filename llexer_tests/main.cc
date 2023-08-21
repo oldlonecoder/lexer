10ba4a60a1c21404fcf0deb928d201f9fbd345d8
oldlonecoder <lussier.serge@gmail.com>
2023-08-21 08:46:46 -0400

    Removed dllibcc.  Abandonning plugin for now

--- llexer_tests/main.cc
+++ llexer_tests/main.cc
@@ -1,69 +1,45 @@
 
-#include <dllibcc/dllib.h>
-#include <xio/tokendata.h>
+
+#include <lexer/lexer_color.h>
+
+
+#include <lexer/lexer_color.h>
+
+
 
 auto main(int argc, char** argv) -> int
 {
+    xio::token_data::collection tokens;
 
-    dll::dll_file file(nullptr, "lexer");
-    
-    if (file.open() != book::rem::ok)
+    std::string code = "result= 5 * 4ac(3 + 2b);";
+    lexer lex;
+    lex.config() =
     {
-        book::rem::push_message(HERE) << " lexer plugin failed ... ;(";
-        book::rem::clear(nullptr);
+        code.c_str(),
+        &tokens
+    };
+
+    auto R = lex();
+    if(R != book::rem::accepted)
+    {
+        std::cout << "lexer failed. See error descriptions in the book logs.\n";
         return -1;
     }
 
-    xio::token_data::collection tokens;
-    std::string code = "result= 5 * 4ac(3 + 2b);";
-    //...
-    book::rem::code c = file.lib()->call<book::rem::code>("assign_config");
-    if (c != book::rem::accepted) return -1;
-    file.lib()->execute();
 
-    book::rem::push_message(HERE) << " closing the lexer library:";
-    auto R =  file.close();
+    for(auto const& t : tokens)
+    {
+        std::cout << t.details(true) << '\n';
+    }
+
+    lexer_color lc;
+    lc << lex.config();
+    // for now ignore lc rem::code return value
+    rem::push_message(HERE) << "Lexer & lexer_color tests results:";
+    rem::out() << lc.Product() << '\n';
+    rem::out() << lc.mark(tokens[5]) << '\n';
+
     book::rem::clear(nullptr);
-    return R;
-}
 
-//#include <lexer/lexer_color.h>
-//
-//
-//
-//auto main(int argc, char** argv) -> int
-//{
-//    xio::token_data::collection tokens;
-//
-//    std::string code = "result= 5 * 4ac(3 + 2b);";
-//    lexer lex;
-//    lex.config() =
-//    {
-//        code.c_str(),
-//        &tokens
-//    };
-//
-//    auto R = lex();
-//    if(R != book::rem::accepted)
-//    {
-//        std::cout << "lexer failed. See error descriptions in the book logs.\n";
-//        return -1;
-//    }
-//
-//
-//    for(auto const& t : tokens)
-//    {
-//        std::cout << t.details(true) << '\n';
-//    }
-//
-//    lexer_color lc;
-//    lc << lex.config();
-//    // for now ignore lc rem::code return value
-//    rem::push_message(HERE) << "Lexer & lexer_color tests results:";
-//    rem::out() << lc.Product() << '\n';
-//    rem::out() << lc.mark(tokens[5]) << '\n';
-//
-//    book::rem::clear(nullptr);
-//
-//    return 0;
-//}
+    return 0;
+}
